242d998158eb42358ae849f8454492ea04909b09
watklangworawihan <wat.klang.worawihan@gmail.com>
2016-03-07 21:22:57 +0700

    Wat Klang Worawihan Website
    v0.1

---
+++ js/main.js
@@ -0,0 +1,10 @@
+﻿
+$(document).ready(function(){
+	$(document).click(function (event){
+		var clickover = $(event.target);
+		var _opened = $(".navbar-collapse").hasClass("in");
+		if(_opened === true && !clickover.hasClass("navbar-toggle")){
+			$("button.navbar-toggle").click();
+		}
+	});
+});
